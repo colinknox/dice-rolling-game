8c49807cd4b19c77e1fade5aa24e88441a03d28d
colinknox <colinwknox@gmail.com>
2026-06-17 21:44:32 -0400

    Wrote up sudo code

---
+++ dice_rolling_game.py
@@ -0,0 +1,11 @@
+# Loop
+    # Ask user "Roll dice? (y/n)" 
+    # If user answer == "y":
+        # Randomize dice roll 1
+        # Randomize dice roll 2
+        # Print (dice roll 1/dice roll 2)
+    # If user answer == "n":
+        # Print "Thanks for playing"
+        # break
+    # If user answer not valid choice:
+        # Print "Invalid choice!"
