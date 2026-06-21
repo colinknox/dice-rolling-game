c46d1d29da6b491f6db2fa4caeeaa9c32f3847a1
colinknox <colinwknox@gmail.com>
2026-06-21 10:23:52 -0400

    added counter for every time user rolls dice

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -1,4 +1,6 @@
 import random
+    
+roll_counter = 0
 
 while True:
     user_choice = input("Roll the dice? (y/n): ").lower()
@@ -8,9 +10,16 @@ while True:
         for roll in range(roll_quantity):
             roll_result = random.randint(1, 6)
             dice_rolls_tuple += (roll_result,)
+        roll_counter += 1
         print(dice_rolls_tuple)
     elif user_choice == "n":
         print("Thanks for playing!")
         break
     else:
         print("Invalid choice!")
+
+    if roll_counter == 1:
+        print(f"You have rolled {roll_counter} time.")
+    else:
+        print(f"You have rolled {roll_counter} times.")
+        
